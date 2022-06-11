84f13f6e4648b81f7ba28181634e05dd8be2b8d7
forbet <matthewcaiuni@gmail.com>
2022-06-10 17:00:34 -0700

    redesigned entire webapp, made navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,39 +1,47 @@
+import { useState } from "react";
 import "./NavBar.css";
 
 function NavBar() {
+  const [mobileNav, setMobileNav] = useState(false);
+  const toggleMobileNav = () => {
+    setMobileNav(!mobileNav);
+  };
+
   return (
     <div className="header-container">
       <span className="logo"> ReMind</span>
-        <ul className="ul-container">
+      <button
+        onClick={toggleMobileNav}
+        className={mobileNav ? "mobile-nav-active" : "mobile-nav-toggle"}
+        aria-controls="navigation-container account-container"
+        aria-expanded="false"
+      >
+        <span className="sr-only">Menu</span>
+      </button>
+      <div className={mobileNav ? "primary-navigation-active" : "primary-navigation"}>
+        <ul
+          id="navigation-container"
+          className="navigation-container">
           <li>
-            <a>
-              Home
-            </a>
+            <a>Home</a>
           </li>
           <li>
-            <a>
-               About Us
-            </a>
+            <a>About</a>
           </li>
           <li>
-            <a>
-               Features
-            </a>
+            <a>Features</a>
           </li>
           <li>
-            <a>
-               Contact
-            </a>
+            <a>Contact</a>
           </li>
         </ul>
-        <div className="account-container">
-            <a>
-                Log In
-            </a>
-            <a>
-                Sign Up
-            </a>
+        <div
+          id="account-container"
+          className="account-container">
+          <a>Log In</a>
+          <a>Sign Up</a>
         </div>
+      </div>
     </div>
   );
 }
