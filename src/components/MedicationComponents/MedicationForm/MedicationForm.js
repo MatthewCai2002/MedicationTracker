8272bbd4750fde361d26af0ce11fdb15a480c785
forbet <matthewcaiuni@gmail.com>
2022-07-16 21:38:38 -0700

    added state to form inputs, added handling for form inputs

--- src/components/MedicationComponents/MedicationForm/MedicationForm.js
+++ src/components/MedicationComponents/MedicationForm/MedicationForm.js
@@ -1,25 +1,52 @@
+import { useState } from "react";
 import Card from "../../Utility/Card/Card";
 import "./MedicationForm.css";
 
 function NewMedication() {
+	const [medName, setMedName] = useState("");
+	const [dosage, setDosage] = useState("");
+
 	const nameChangeHandler = (event) => {
-		console.log(event.target.value);
+		setMedName(event.target.value);
 	};
-  
+
+	const dosageChangeHandler = (event) => {
+		setDosage(event.target.value);
+	};
+
+	const submitHandler = (event) => {
+		event.preventDefault();
+		const medData = {
+			name: medName,
+			dosage: dosage,
+		};
+		setDosage('')
+		setMedName('')
+		console.log(medData)
+	};
+
 	return (
 		<Card className="add-med-container">
-			<form className="add-med-form">
+			<form className="add-med-form" onSubmit={submitHandler}>
 				<div>
 					<label htmlFor="medName"> Medication Name: </label>
 					<input
 						id="medName"
 						type="text"
+						value={medName}
 						onChange={nameChangeHandler}
 					/>
 				</div>
 				<div>
 					<label htmlFor="dosage"> Dosage in mg: </label>
-					<input id="dosage" type="number" min="0" step="0.1" />
+					<input
+						id="dosage"
+						type="number"
+						min="0"
+						step="0.1"
+						value={dosage}
+						onChange={dosageChangeHandler}
+					/>
 				</div>
 				<button type="submit" className="submit-med-btn">
 					Add Medication
